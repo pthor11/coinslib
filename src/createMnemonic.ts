e49f39bea23a8c615f43f07f96f62c0be83fa106
phuonglm <phuonglm@me.com>
2020-05-29 18:00:32 +0700

    update

--- src/createMnemonic.ts
+++ src/createMnemonic.ts
@@ -1,13 +1,13 @@
 import * as Random from 'expo-random'
 import wordlist from './wordlist_eng'
 
-const createMnemonic = async () => {
+export const createMnemonic = async (): Promise<string[]> => {
     try {
-        const randoms = []
+        const randoms: number[] = []
         const quantity = 24
         while (randoms.length < quantity) {
             let randomBytes = await Random.getRandomBytesAsync(100)
-            let number = (randomBytes["0"] * 16 + randomBytes["1"]) % 2048
+            let number = (randomBytes[0] * 16 + randomBytes[1]) % 2048
             if (!randoms.includes(number)) randoms.push(number)
         }
         return randoms.map(random => wordlist[random])
@@ -16,5 +16,3 @@ const createMnemonic = async () => {
         return Promise.reject({ code: 9000 })
     }
 }
-
-export { createMnemonic }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
-const Mnemonic = require ("bitcore-mnemonic");
-
-export const createMnemonic = () => {
-    const code = new Mnemonic(256, Mnemonic.Words.ENGLISH)
-    // console.log({code: code.toString().split(' ').length, hex: code.toSeed().toString('hex')});   
-    return code.toString().split(' ')
-}
-
-// createMnemonic() 
+export * from "./createMnemonic"
